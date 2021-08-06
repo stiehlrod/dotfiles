82bc99609cbb830c104f435e69d01d3ecd96eb43
Jennica Stiehl <25069483+stiehlrod@users.noreply.github.com>
2021-08-06 13:58:04 -0600

    Adjusts brewfile adds to gitigore file

---
+++ Brewfile
@@ -0,0 +1,19 @@
+# Taps
+tap "homebrew/bundle"
+tap "homebrew/cask"
+tap "homebrew/core"
+
+# Brews (Packages)
+brew "bat"
+brew "exa"
+brew "httpie"
+brew "node"
+brew "openjdk"
+brew "rbenv"
+brew "sbt"
+brew "scala"
+
+# Casks (Applications)
+cask "dropbox"
+# cask "google-chrome"
+cask "visual-studio-code"
--- set_up_homebrew.zsh
+++ set_up_homebrew.zsh
@@ -4,8 +4,11 @@ echo "\nStarting Homebrew Setup >>>\n"
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install httpie
-brew install bat
+# brew install httpie
+# brew install bat
 
-brew install google-chrome
-brew install visual-studio-code
+# brew install --no-quarantine google-chrome
+# brew install --no-quarantine visual-studio-code
+# brew install --no-quarantine alfred
+
+brew bundle --verbose
